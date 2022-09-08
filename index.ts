c7844d75774b546408850d994407036aaddc31a9
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-09-08 11:41:55 +0200

    Dotenv and cookieParser were configured.

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import express, {json} from "express";
+import dotenv from "dotenv";
 import cors from 'cors';
 import 'express-async-errors';
 import { handleError } from "./utils/errors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+dotenv.config();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -11,6 +14,7 @@ app.use(cors({
 }));
 app.use(json());
 
+app.use(cookieParser());
 app.use(handleError);
 
 app.listen(3001, '0.0.0.0',() => {
