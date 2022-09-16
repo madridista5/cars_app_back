61b581a306e68a3df0aa55e33e6b2f22fb43ff2b
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-09-16 11:28:28 +0200

    Route for addImage function has been prepared.

--- routes/images.router.ts
+++ routes/images.router.ts
@@ -1,3 +1,7 @@
 import {Router} from "express";
+import {addImage} from "../controllers/image.controller";
 
 export const imagesRouter = Router();
+
+imagesRouter
+    .post('/add/:id/:carId', addImage);
