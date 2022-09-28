9707cd45b5dd7634498f234b7e34e8a94df38350
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-09-28 10:50:10 +0200

    Class WatchRecord was created.

--- controllers/watch.controller.ts
+++ controllers/watch.controller.ts
@@ -1,3 +1,9 @@
-export const addWatch = () => {
+import {NextFunction, Request, Response} from "express";
 
+export const addWatch = (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    }
 };
