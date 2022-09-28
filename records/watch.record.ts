4177bf0fabd2f7b4946afe3146bcaf65c1d05a9e
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-09-28 11:00:21 +0200

    Method insert was created for WatchRecord.

--- records/watch.record.ts
+++ records/watch.record.ts
@@ -1,3 +1,28 @@
-export class WatchRecord {
+import {WatchEntity} from "../types";
+import {v4 as uuid} from "uuid";
+import {pool} from "../utils/db";
 
+export class WatchRecord implements WatchEntity {
+    id?: string;
+    userId: string;
+    carId: string;
+
+    constructor(obj: WatchEntity) {
+        this.id = obj.id;
+        this.userId = obj.userId;
+        this.carId = obj.carId;
+    }
+
+    async insert(): Promise<string> {
+        if(!this.id) {
+            this.id = uuid();
+        }
+
+        await pool.execute("INSERT INTO `watch` VALUES(:id, :userId, :carId)", {
+            id: this.id,
+            userId: this.userId,
+            carId: this.carId,
+        });
+        return this.id;
+    }
 }
