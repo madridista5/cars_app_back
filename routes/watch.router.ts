99936037a1f66e9906b2616ca822741fabb8f65d
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-09-28 10:47:03 +0200

    Watch controller was created.

--- routes/watch.router.ts
+++ routes/watch.router.ts
@@ -1,4 +1,8 @@
 import {Router} from "express";
+import {verifyUser} from "../utils/verifyToken";
+import {addWatch} from "../controllers/watch.controller";
 
 export const watchRouter = Router();
 
+watchRouter
+    .post('/add/:userId', verifyUser, addWatch);
